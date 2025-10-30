84898eab87dc6cfbc02c08f4f32dbb7329b363ee
DELIN SHAJI <Delin.john@algo8.ai>
2025-10-30 12:13:17 +0530

    changed  d-m

--- data_fetcher/mt_data.py
+++ data_fetcher/mt_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from config import load_engine
 from sqlmodel import create_engine
+from datetime import datetime, timedelta
 
 
 
@@ -12,17 +13,25 @@ engine = create_engine(engine_name)
 
 class DataReaderMt:
 
-  
-
     def read_mt_pwani_data(self):
-        query = "SELECT * FROM pwani_marketing.mt_pwani_data_cleaned"
+        # Calculate the cutoff date (4 months ago)
+        cutoff_date = (datetime.now() - timedelta(days=120)).strftime('%Y-%m-%d')
+        
+        query = f"""
+            SELECT *
+            FROM pwani_marketing.mt_pwani_data_cleaned
+            WHERE date >= '{cutoff_date}'
+        """
         data = pd.read_sql_query(query, engine)
- 
         return data
-       
 
     def read_mt_competitor_data(self):
-        query = "SELECT * FROM pwani_marketing.mt_competitor_data_cleaned"
-        df = pd.read_sql_query(query, engine)
+        cutoff_date = (datetime.now() - timedelta(days=120)).strftime('%Y-%m-%d')
 
+        query = f"""
+            SELECT *
+            FROM pwani_marketing.mt_competitor_data_cleaned
+            WHERE date >= '{cutoff_date}'
+        """
+        df = pd.read_sql_query(query, engine)
         return df
